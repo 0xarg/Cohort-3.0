97814d3f633217e8f7b0b05117176d74d6e376c3
anurag8484 <anuragpoonia.ap@gmail.com>
2026-01-06 22:30:35 +0530

    learn: viem

--- Web3/eth-fe-week-11/app/page.tsx
+++ Web3/eth-fe-week-11/app/page.tsx
@@ -1,22 +1,31 @@
 "use client";
+import axios from "axios";
+import React, { useCallback, useState } from "react";
+import { createPublicClient, http } from "viem";
+import { getBalance } from "viem/actions";
+import { mainnet } from "viem/chains";
 
-import { useQuery } from "@tanstack/react-query";
-
-const Page = () => {
-  const { data, isPending, error } = useQuery({
-    queryKey: ["todos"],
-    queryFn: async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
-      if (!res.ok) throw new Error("Failed to fetch");
-      return res.json();
-    },
-    refetchInterval: 10 * 1000,
+const page = () => {
+  const [balance, setBalance] = useState<bigint>();
+  const client = createPublicClient({
+    chain: mainnet,
+    transport: http(),
   });
 
-  if (isPending) return <div>Loading...</div>;
-  if (error) return <div>Error</div>;
+  const fetchBalace = useCallback(async () => {
+    const res = await getBalance(client, {
+      address: "0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD",
+    });
+    setBalance(res);
+    console.log(res);
+  }, [client]);
 
-  return <ul>{JSON.stringify(data)}</ul>;
+  return (
+    <div>
+      <p>{balance}</p>
+      <button onClick={() => fetchBalace()}>Get Balance</button>
+    </div>
+  );
 };
 
-export default Page;
+export default page;
