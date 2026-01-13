c3b4ec7404dff30acf90064a3058137f65491120
anurag8484 <anuragpoonia.ap@gmail.com>
2026-01-14 00:21:54 +0530

    add: dialog logic

--- Web3/eth-client/app/WalletOptions.tsx
+++ Web3/eth-client/app/WalletOptions.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { config } from "@/lib/config";
+import { useState } from "react";
 import {
   useBalance,
   useConnect,
@@ -13,37 +16,44 @@ export function WalletOptions({ children }: { children: React.ReactNode }) {
   const connection = useConnection();
   const connectors = useConnectors();
   const { connector } = getConnection(config);
+  const disconnect = useDisconnect(); // Use the hook for disconnection
+
   const balance = useBalance({ address: connection.address });
-  const handleDisconnect = async () => {
-    const res = await disconnect(config, {
-      connector,
-    });
-  };
+  const [open, setOpen] = useState(false);
   return (
     <>
-      {connection.address ? (
+      {!open && !connection.address && (
+        <Button onClick={() => setOpen(true)}>Connect to Wallet</Button>
+      )}
+
+      {!connection.address &&
+        open &&
         connectors.map((connector) => (
           <button
             className="py-1 px-2 cursor-pointer hover:border-red-600 hover:border bg-neutral-600 m-5 rounded-md"
             key={connector.uid}
-            onClick={() => connect.mutate({ connector })}
+            onClick={() => {
+              connect.mutate({ connector });
+              setOpen(false);
+            }}
           >
             {connector.name}
           </button>
-        ))
-      ) : (
-        <div className="flex gap-3">
+        ))}
+      {connection.address && (
+        <div className="flex gap-3 justify-center items-center">
           <span>
             You are connected! <span> {connection.address}</span>
           </span>
           <button
             className="py-1 px-2 cursor-pointer hover:border-red-600 hover:border bg-neutral-600 m-5 rounded-md"
-            onClick={() => handleDisconnect()}
+            onClick={() => disconnect.mutate({ connector })}
           >
             Disconnect
           </button>
         </div>
       )}
+
       <br />
       {children}
     </>
