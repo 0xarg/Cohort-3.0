cb39ea963b6c6f0ecf76eddd80085eddcd7037cb
anurag8484 <anuragpoonia.ap@gmail.com>
2026-01-14 10:30:01 +0530

    learn: readContract hook

--- Web3/eth-client/app/page.tsx
+++ Web3/eth-client/app/page.tsx
@@ -3,16 +3,20 @@ import { config } from "@/lib/config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { WalletOptions } from "./WalletOptions";
+import TotalSupply from "@/components/TotalSupply";
 
 function page() {
   const client = new QueryClient();
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
-        <div className="flex justify-center items-center h-screen w-full">
+        <div className="flex justify-center flex-col  h-screen w-full">
           <WalletOptions>
             <></>
           </WalletOptions>
+          <div>
+            <TotalSupply />
+          </div>
         </div>
       </QueryClientProvider>
     </WagmiProvider>
