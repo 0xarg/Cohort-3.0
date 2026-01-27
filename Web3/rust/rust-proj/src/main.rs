fcaf01f0d0a64fad0948413fd6895904b5b5f2fb
anurag8484 <anuragpoonia.ap@gmail.com>
2026-01-27 21:56:35 +0530

    learn: adv topics in rust

--- Web3/rust/rust-proj/src/main.rs
+++ Web3/rust/rust-proj/src/main.rs
@@ -1,31 +1,21 @@
+use std::fs;
+
+enum Result<A, B> {
+    Ok(A),
+    Err(B),
+}
+
 fn main() {
-    stack_fn(); // Call the function that uses stack memory
-    heap_fn(); // Call the function that uses heap memory
-    update_string(); // Call the function that changes size of variable at runtime
-}
+    let res = fs::read_to_string("example.txt");
 
-fn stack_fn() {
-    // Declare a few integers on the stack
-    let a = 10;
-    let b = 20;
-    let c = a + b;
-    println!("Stack function: The sum of {} and {} is {}", a, b, c);
-}
-
-fn heap_fn() {
-    // Create a string, which is allocated on the heap
-    let s1 = String::from("Hello");
-    let s2 = String::from("World");
-    let combined = format!("{} {}", s1, s2);
-    println!("Heap function: Combined string is '{}'", combined);
-}
-
-fn update_string() {
-    // Start with a base string on the heap
-    let mut s = String::from("Initial string");
-    println!("Before update: {}", s);
-
-    // Append some text to the string
-    s.push_str(" and some additional text");
-    println!("After update: {}", s);
+    // match res {
+    //     Ok(content) => {
+    //         println!("File content: {}", content);
+    //     }
+    //     Err(err) => {
+    //         println!("Error {}", err);
+    //     }
+    // }
+    println!("{}", res);
+    println!("Hey!")
 }
